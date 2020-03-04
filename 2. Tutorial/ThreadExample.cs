51797ce20cbfb6b127ad993816cb564a70468cfe
mayuripandey <mayuripandey4@gmail.com>
2020-03-04 23:34:59 +0530

    SSE Ubung

---
+++ 2. Tutorial/ThreadExample.cs
@@ -0,0 +1,46 @@
+﻿// Course Material "Protokolle Verteilte Systeme"
+// (c) 2008 by Professur Verteilte und Selbstorganisierende Rechnersysteme, TUC
+
+using System;
+using System.Threading;
+
+namespace Vsr.Teaching.Pvs.Sample
+{
+    /// <summary>
+    /// A sample program that demonstrates the use of threads.
+    /// </summary>
+    class ThreadExample
+    {
+        static void Method()        
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("b");
+                // wait for 0.1s
+                Thread.Sleep(100);
+            }
+        }
+
+        /// <summary>
+        /// The main program routine.
+        /// </summary>        
+        static void Main(string[] args)
+        {
+            // start a parallel thread
+            Thread t = new Thread(Method);
+            t.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("a");
+                // wait for 0.1s
+                Thread.Sleep(100);
+            }
+
+            // wait for thread to end
+            t.Join();
+            Console.ReadLine();
+        }        
+      
+    }
+}
---
+++ 4. Tutorial/TextParsingExample.cs
@@ -0,0 +1,58 @@
+﻿// Course Material "Software Service Engineering"
+// (c) 2013 by Distributed and Self-organizing Systems Group, TUC
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace Vsr.Teaching.SSE.Sample
+{
+    /// <summary>
+    /// A sample program that demonstrates the parsing of text.
+    /// </summary>
+    class TextParsingExample
+    {
+        /// <summary>
+        /// The main program routine.
+        /// </summary>        
+        static void Main(string[] args)
+        {
+            string myText = "This is a sample sentence.";
+
+            // working with character positions
+            int pos = myText.IndexOf(" a ");
+            Console.WriteLine(myText.Substring(0, pos));
+            Console.WriteLine(Environment.NewLine);
+
+            // splitting text at certain characters
+            string[] words = myText.Split(' ');
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine(Environment.NewLine);
+
+            // matching with regular expressions
+            Match match = Regex.Match(myText, @"^(?<a>[^s]*s)(?<b>[^p]*p)(?<c>.*)$");
+            Console.WriteLine(match.Groups["a"].Value); // This
+            Console.WriteLine(match.Groups["b"].Value); //  is a samp
+            Console.WriteLine(match.Groups["c"].Value); // le sentence.
+            Console.WriteLine(Environment.NewLine);
+
+            // parsing numbers
+            myText = "41";
+            int myNumber = Int32.Parse(myText);
+            Console.WriteLine(myNumber + 1);
+            Console.WriteLine(Environment.NewLine);
+
+            // convert between numbers and hexadecimal strings
+            string hex = Convert.ToByte(42).ToString("x");
+            int number = Int32.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine("hex: {0}, dec: {1}", hex, number);
+            Console.WriteLine(Environment.NewLine);
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
